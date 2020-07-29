c7eff56b62ec120a996dc4e4fc2168d7046d58ea
Randy Chase <randy.chase12@gmail.com>
2020-07-28 19:48:32 -0500

    some patches to the boxbin func

--- drpy/util/util.py
+++ drpy/util/util.py
@@ -10,8 +10,9 @@ def percentile(n):
     percentile_.__name__ = 'percentile_%s' % n
     return percentile_
 
+#determine unconditional mean, sum R in each bin. But then devide by master counts
 def boxbin(x,y,xedge,yedge,c=None,figsize=(5,5),cmap='viridis',mincnt=10,vmin=None,vmax=None,edgecolor=None,powernorm=False,
-           ax=None,normed=False,method='mean',quantile=None,alpha=1.0,cbar=True):
+           ax=None,normed=False,method='mean',quantile=None,alpha=1.0,cbar=True,unconditional=False,master_count=np.array([])):
     
     """ This function will grid data for you and provide the counts if no variable c is given, or the median if 
     a variable c is given. In the future I will add functionallity to do the median, and possibly quantiles. 
@@ -38,22 +39,49 @@ def boxbin(x,y,xedge,yedge,c=None,figsize=(5,5),cmap='viridis',mincnt=10,vmin=No
     #note on digitize. bin 0 is outside to the left of the bins, bin -1 is outside to the right
     ind1 = np.digitize(x,bins = xedge) #inds of x in each bin
     ind2 = np.digitize(y,bins = yedge) #inds of y in each bin
-
+    
+    
+    #drop points outside range 
+    outsideleft = np.where(ind1 != 0)
+    ind1 = ind1[outsideleft]
+    ind2 = ind2[outsideleft]
+    if c is None:
+        pass
+    else:
+        c = c[outsideleft]
+        
+    outsideright = np.where(ind1 != len(xedge))
+    ind1 = ind1[outsideright]
+    ind2 = ind2[outsideright]
+    if c is None:
+        pass
+    else:
+        c = c[outsideright]
+        
+    outsideleft = np.where(ind2 != 0)
+    ind1 = ind1[outsideleft]
+    ind2 = ind2[outsideleft]
+    if c is None:
+        pass
+    else:
+        c = c[outsideleft]
+    outsideright = np.where(ind2 != len(yedge))
+    ind1 = ind1[outsideright]
+    ind2 = ind2[outsideright]
+    if c is None:
+        pass
+    else:
+        c = c[outsideright]
     
 
     if c is None:
-        c = np.zeros(len(x))
-        df = pd.DataFrame({'x':ind1,'y':ind2,'c':c})
+        c = np.zeros(len(ind1))
+        df = pd.DataFrame({'x':ind1-1,'y':ind2-1,'c':c})
         df2 = df.groupby(["x","y"]).count()
         df = df2.where(df2.values >= mincnt).dropna()
         C = np.ones([xedge.shape[0]-1,yedge.shape[0]-1])*-9999
         for i,ii in enumerate(df.index.values):
-            #dont plot points outside the bounds
-            if (ii[0]==0) or (ii[1] == 0) or (ii[0] >= len(xedge)-1) or (ii[1] >= len(yedge)-1):
-                pass
-            else:
-                #shift indexs so that bin 1 is actually bin 0. 
-                C[ii[0]-1,ii[1]-1] = df.c.values[i]
+            C[ii[0],ii[1]] = df.c.values[i]
         C = np.ma.masked_where(C == -9999,C)
         
         if normed:
@@ -73,15 +101,52 @@ def boxbin(x,y,xedge,yedge,c=None,figsize=(5,5),cmap='viridis',mincnt=10,vmin=No
             
             if cbar:
                 cbar = plt.colorbar(pm,ax=ax)
+            else:
+                cbar = pm 
         else:
             pm = ax.pcolormesh(xedge,yedge,C.transpose(),cmap=cmap,vmin=vmin,vmax=vmax,edgecolor=edgecolor,alpha=alpha)
             if cbar:
                 cbar = plt.colorbar(pm,ax=ax)
+            else:
+                cbar = pm 
             
         return ax,cbar,C
     
+    elif unconditional:
+    
+        df = pd.DataFrame({'x':ind1-1,'y':ind2-1,'c':c})
+        if method=='mean':
+            df2 = df.groupby(["x","y"])['c'].sum()
+            
+        df3 = df.groupby(["x","y"]).count()
+        df2 = df2.to_frame()
+        df2.insert(1,'Count',df3.values)
+        df = df2.where(df2.Count >= mincnt).dropna()
+        C = np.ones([xedge.shape[0]-1,yedge.shape[0]-1])
+        for i,ii in enumerate(df.index.values):
+            C[ii[0],ii[1]] = df.c.values[i]
+                
+        C = C/master_count.values
+
+        if ax is None:
+            fig = plt.figure(figsize=(5,5))
+            ax = plt.gca()
+        else:
+            pass
+        
+        if powernorm:
+            pm = ax.pcolor(xedge,yedge,C.transpose(),cmap=cmap,vmin=vmin,vmax=vmax,norm=colors.PowerNorm(gamma=0.5),alpha=alpha)
+            if cbar:
+                cbar = plt.colorbar(pm,ax=ax)
+        else:
+            
+            pm = ax.pcolor(xedge,yedge,C.transpose(),cmap=cmap,vmin=vmin,vmax=vmax,alpha=alpha)
+            if cbar: 
+                cbar = plt.colorbar(pm,ax=ax)
+        
+        
     else:
-        df = pd.DataFrame({'x':ind1,'y':ind2,'c':c})
+        df = pd.DataFrame({'x':ind1-1,'y':ind2-1,'c':c})
         if method=='mean':
             df2 = df.groupby(["x","y"])['c'].mean()
         elif method=='std':
@@ -103,12 +168,7 @@ def boxbin(x,y,xedge,yedge,c=None,figsize=(5,5),cmap='viridis',mincnt=10,vmin=No
         df = df2.where(df2.Count >= mincnt).dropna()
         C = np.ones([xedge.shape[0]-1,yedge.shape[0]-1])*-9999
         for i,ii in enumerate(df.index.values):
-            #dont plot points outside the bounds
-            if (ii[0]==0) or (ii[1] == 0) or (ii[0] >= len(xedge)-1) or (ii[1] >= len(yedge)-1):
-                pass
-            else:
-                #shift indexs so that bin 1 is actually bin 0.
-                C[ii[0]-1,ii[1]-1] = df.c.values[i]
+            C[ii[0],ii[1]] = df.c.values[i]
 
         C = np.ma.masked_where(C == -9999,C)
 
@@ -122,10 +182,14 @@ def boxbin(x,y,xedge,yedge,c=None,figsize=(5,5),cmap='viridis',mincnt=10,vmin=No
             pm = ax.pcolor(xedge,yedge,C.transpose(),cmap=cmap,vmin=vmin,vmax=vmax,norm=colors.PowerNorm(gamma=0.5),alpha=alpha)
             if cbar:
                 cbar = plt.colorbar(pm,ax=ax)
+            else:
+                cbar = pm
         else:
             
             pm = ax.pcolor(xedge,yedge,C.transpose(),cmap=cmap,vmin=vmin,vmax=vmax,alpha=alpha)
             if cbar: 
                 cbar = plt.colorbar(pm,ax=ax)
+            else:
+                cbar = pm 
             
     return ax,cbar,C
